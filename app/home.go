65a6330c8f0fbb9692cefbd9952ed0b56ff6cacd
André Karrlein <andre@karrlein.com>
2023-08-17 18:17:09 +0200

    Update home.go

--- app/home.go
+++ app/home.go
@@ -20,8 +20,7 @@ func (home *home) Render() app.UI {
 						app.P().Text("Unsere Lern-App ist die perfekte Lösung für Schüler, die ihr Wissen in verschiedenen Fächern erweitern möchten. Mit einer Vielzahl von Übungsaufgaben kannst du deine Leistungen verbessern."),
 						app.Br(),
 						app.Br(),
-						app.H2().Text("Bald verfügbar!"),
-						//app.Raw("<a href='https://apps.apple.com/de/app/studybear/id6449251194?itsct=apps_box_badge&amp;itscg=30200' style='display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;'><img src='https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/de-de?size=250x83&amp;releaseDate=1643673600' alt='Download on the App Store' style='border-radius: 13px; width: 250px; height: 83px;'></a>"),
+						app.Raw("<a href="https://apps.apple.com/de/app/studybear/id6449251194?itsct=apps_box_badge&amp;itscg=30200" style="display: inline-block; overflow: hidden; border-radius: 13px; width: 250px; height: 83px;"><img src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/de-de?size=250x83&amp;releaseDate=1693785600" alt="Download on the App Store" style="border-radius: 13px; width: 250px; height: 83px;"></a>"),
 					),
 					app.Div().Class("img-box").Body(
 						app.Img().Src("/web/images/logo.jpeg"),
